dc1921434b41ddb4e806274a34d09552842ab57d
rishikesh tawade <134831594+RishikeshTawade1998@users.noreply.github.com>
2023-05-28 13:18:50 +0530

    Add files via upload

---
+++ Data_Types.java
@@ -0,0 +1,29 @@
+class Data_Types
+{
+	public static void main(String args[])
+	{
+	             // Data type is declared  // size 				
+
+		boolean a=true;  	//1 byte
+		char b='a';	//2 byte
+		String c="rishi";	// Non Premative Data Type
+		byte  d=2;		//1 byte
+		short e=3;		//2 byte
+		int f=4;		//4 byte
+		long g=5l;		//8 byte
+		float h=6.0f;	//4 byte
+		double i=7.1;	//8 byte
+
+		// printing data types 
+		System.out.println("Boolean data Type : "+a);
+		System.out.println("Character data type : "+b);
+		System.out.println("String data type : "+c);
+		System.out.println("Byte data type : "+d);
+		System.out.println("Short data Type : "+e);
+		System.out.println("Integer data type : "+f);
+		System.out.println("Long data type : "+g);
+		System.out.println("Float data type : "+h);
+		System.out.println("Double data type : "+i);
+	}
+}
+		

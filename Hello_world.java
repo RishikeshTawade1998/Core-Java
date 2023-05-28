e38935be32f54099e503a9ed82846d61002d2d9c
rishikesh tawade <134831594+RishikeshTawade1998@users.noreply.github.com>
2023-05-28 12:59:35 +0530

    Add files via upload

---
+++ Hello_world.java
@@ -0,0 +1,10 @@
+// class is created
+public class Hello_world
+{
+	// main method
+	public static void main(String args[])
+		{
+			//printing statment
+			System.out.println("Hello World");
+		}
+}
